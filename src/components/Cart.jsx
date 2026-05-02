db06a58ca717d37e918acc5a91acf4ae164509d0
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-05-02 22:58:15 +0530

    Add react-icons dependency and update Cart and ProductCard components
    - Added "react-icons" to dependencies in package.json
    - Enhanced Cart component to display a message when empty and list items when populated
    - Updated ProductCard component to properly render product details and add to cart functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,29 @@
-const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+import CartItem from "./CartItem";
 
-export default Cart
+const Cart = ({ cart, onUpdateQuantity, onRemove, total }) => {
+  if (cart.length === 0) {
+    return (
+      <div className="cart empty">
+        <h2>Cart is Empty!! Add some products</h2>
+      </div>
+    );
+  }
+  return (
+    <div className="cart">
+      <h2>Shopping Cart</h2>
+      {cart.map((item) => (
+        <CartItem
+          key={item.id}
+          item={item}
+          onUpdateQuantity={onUpdateQuantity}
+          onRemove={onRemove}
+        />
+      ))}
+      <div>
+        <h2>Total :</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,15 @@
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+import { FaShoppingCart } from "react-icons/fa";
+const ProductCard = ({ product, onAddToCart }) => {
+  return (
+    <div className="product">
+      <h2>{product.name}</h2>
+      <h3>{product.price}</h3>
+      <button onClick={onAddToCart}>
+        <FaShoppingCart />
+        Add to Cart
+      </button>
+    </div>
+  );
 };
 
 export default ProductCard;
